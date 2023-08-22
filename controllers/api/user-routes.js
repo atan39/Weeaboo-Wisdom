470ad7e67ce70e039c0f2396b8a50a8cf5f8854e
Louis Antolin <ragnaman.exe@gmail.com>
2023-08-21 17:45:27 -0700

    removed useless comments

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,20 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-//insomnia backend use to check
-/*
-router.get('/', (req, res) => {
-  User.findAll({
-    })
-    .then((userData) => {
-      res.status(200).json(userData);
-    })
-    .catch( (err) => {
-      res.status(500).json(err);
-  })
-});
-*/
-
 //posts user
 router.post('/', async (req, res) => {
   try {
